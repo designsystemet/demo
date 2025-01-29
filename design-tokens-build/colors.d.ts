662c0c20f86d5cad7c373b60493ddc9e63aa588e
Michael Marszalek <mimarz@gmail.com>
2025-01-29 09:09:45 +0100

    updates for demo

--- design-tokens-build/colors.d.ts
+++ design-tokens-build/colors.d.ts
@@ -1,9 +1,7 @@
-import type { MainAndSupportColors as BaseCustomColors } from '@digdir/designsystemet-react/colors';
+import type {} from '@digdir/designsystemet-react/colors';
 
 declare module '@digdir/designsystemet-react/colors' {
-  export interface MainAndSupportColors extends BaseCustomColors {
-    primary: never;
-    dark-green: never;
-    special: never;
+  export interface MainAndSupportColors {
+    sweet: never;
   }
 }
