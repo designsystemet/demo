9ac3886f16beb4b282274d3be7249ce1a1a4ca0d
Michael Marszalek <mimarz@gmail.com>
2025-03-12 09:25:39 +0100

    update to v1

--- design-tokens-build/colors.d.ts
+++ design-tokens-build/colors.d.ts
@@ -3,6 +3,5 @@ import type {} from '@digdir/designsystemet-react/colors';
 declare module '@digdir/designsystemet-react/colors' {
   export interface MainAndSupportColors {
     hoved: never;
-    sweet: never;
   }
 }
