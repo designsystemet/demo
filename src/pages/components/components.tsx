cbef87a335d05577a1244a2ca2dfa9c11451e75a
Oddbjørn Øvernes <oddbjorn.overnes@gmail.com>
2025-07-10 14:57:29 +0200

    components: add size and scheme controls

--- src/pages/components/components.tsx
+++ src/pages/components/components.tsx
@@ -1,21 +1,67 @@
-import { Card, Heading } from "@digdir/designsystemet-react";
+import {
+  Card,
+  Fieldset,
+  Heading,
+  type Size,
+  ToggleGroup,
+} from "@digdir/designsystemet-react";
+import { useState } from "react";
 import classes from "./components.module.css";
 import * as samples from "./samples/";
 
+const sizes: Size[] = ["sm", "md", "lg"];
+const colorModes = ["light", "dark", "auto"];
+
 export const Components = () => {
+  const [size, setSize] = useState<Size>("sm");
+  const [colorMode, setColorMode] = useState("auto");
   return (
-    <div className={classes.container}>
-      {Object.entries(samples).map(([name, Component]) => {
-        if (typeof Component === "function") {
-          return (
-            <Card key={name}>
-              <Heading>{name.slice(0, -6)}</Heading>
-              <Component />
-            </Card>
-          );
-        }
-        return null;
-      })}
-    </div>
+    <>
+      <div className={classes.container} data-size="sm">
+        <Fieldset>
+          <Fieldset.Legend>
+            Størrelse
+            <code data-size="xs">(data-size)</code>
+          </Fieldset.Legend>
+          <ToggleGroup value={size} onChange={(val) => setSize(val as Size)}>
+            {sizes.map((size) => (
+              <ToggleGroup.Item key={size} value={size}>
+                {size}
+              </ToggleGroup.Item>
+            ))}
+          </ToggleGroup>
+        </Fieldset>
+        <Fieldset>
+          <Fieldset.Legend>
+            Fargemodus
+            <code data-size="xs">(data-color-scheme)</code>
+          </Fieldset.Legend>
+          <ToggleGroup value={colorMode} onChange={setColorMode}>
+            {colorModes.map((colorMode) => (
+              <ToggleGroup.Item key={colorMode} value={colorMode}>
+                {colorMode}
+              </ToggleGroup.Item>
+            ))}
+          </ToggleGroup>
+        </Fieldset>
+      </div>
+      <div
+        className={classes.container}
+        data-size={size}
+        data-color-scheme={colorMode}
+      >
+        {Object.entries(samples).map(([name, Component]) => {
+          if (typeof Component === "function") {
+            return (
+              <Card key={name}>
+                <Heading>{name.slice(0, -6)}</Heading>
+                <Component />
+              </Card>
+            );
+          }
+          return null;
+        })}
+      </div>
+    </>
   );
 };
