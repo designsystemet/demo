7400d034a2c50808f4968c2eb83b48b782f37b65
Oddbjørn Øvernes <oddbjorn.overnes@gmail.com>
2025-07-16 12:37:33 +0200

    update suggestion story

--- src/pages/components/samples/Suggestion.tsx
+++ src/pages/components/samples/Suggestion.tsx
@@ -5,27 +5,26 @@ import {
   Field,
   Label,
   Paragraph,
-} from '@digdir/designsystemet-react';
-import { useState } from 'react';
+} from "@digdir/designsystemet-react";
+import { useState } from "react";
 
 const DATA_PLACES = [
-  'Sogndal',
-  'Oslo',
-  'Brønnøysund',
-  'Stavanger',
-  'Trondheim',
-  'Bergen',
-  'Lillestrøm',
+  "Sogndal",
+  "Oslo",
+  "Brønnøysund",
+  "Stavanger",
+  "Trondheim",
+  "Bergen",
+  "Lillestrøm",
 ];
 
 function SuggestionSample() {
-  const [value, setValue] = useState<string[]>(['Oslo']);
+  const [value, setValue] = useState<string[]>(["Oslo"]);
   return (
-    <div className='column'>
+    <div className="column">
       <Field>
         <Label>Velg en destinasjon</Label>
         <EXPERIMENTAL_Suggestion>
-          <EXPERIMENTAL_Suggestion.Chips />
           <EXPERIMENTAL_Suggestion.Input />
           <EXPERIMENTAL_Suggestion.Clear />
           <EXPERIMENTAL_Suggestion.List>
@@ -47,10 +46,11 @@ function SuggestionSample() {
         <Label>Velg destinasjoner</Label>
         <EXPERIMENTAL_Suggestion
           multiple
-          value={value}
-          onValueChange={(items) => setValue(items.map((item) => item.value))}
+          selected={value}
+          onSelectedChange={(items) =>
+            setValue(items.map((item) => item.value))
+          }
         >
-          <EXPERIMENTAL_Suggestion.Chips />
           <EXPERIMENTAL_Suggestion.Input />
           <EXPERIMENTAL_Suggestion.Clear />
           <EXPERIMENTAL_Suggestion.List>
@@ -68,15 +68,15 @@ function SuggestionSample() {
           </EXPERIMENTAL_Suggestion.List>
         </EXPERIMENTAL_Suggestion>
       </Field>
-      <Divider style={{ marginTop: 'var(--ds-size-4)' }} />
+      <Divider style={{ marginTop: "var(--ds-size-4)" }} />
 
-      <Paragraph style={{ margin: 'var(--ds-size-2) 0' }}>
-        Valgte reisemål: {value.join(', ')}
+      <Paragraph style={{ margin: "var(--ds-size-2) 0" }}>
+        Valgte reisemål: {value.join(", ")}
       </Paragraph>
 
       <Button
         onClick={() => {
-          setValue(['Sogndal', 'Stavanger']);
+          setValue(["Sogndal", "Stavanger"]);
         }}
       >
         Sett reisemål til Sogndal, Stavanger
