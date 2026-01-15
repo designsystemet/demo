8a9beebfca82986932bf7f3dcc4d6d117aeba27d
Oddbjørn Øvernes <oddbjorn.overnes@gmail.com>
2026-01-15 12:43:43 +0100

    type fix

--- src/pages/components/samples/Avatar.tsx
+++ src/pages/components/samples/Avatar.tsx
@@ -1,21 +1,21 @@
-import { Avatar } from '@digdir/designsystemet-react';
+import { Avatar } from "@digdir/designsystemet-react";
 
 const AvatarSample = () => {
   return (
-    <div className='row'>
-      <Avatar data-size='xs' aria-label='extra small' initials='xs' />
-      <Avatar data-size='xs' aria-label='extra small' />
-      <Avatar data-size='sm' aria-label='small' initials='sm' />
-      <Avatar data-size='sm' aria-label='small' />
-      <Avatar data-size='md' aria-label='medium' initials='md' />
-      <Avatar data-size='md' aria-label='medium' />
+    <div className="row">
+      <Avatar data-size="xs" aria-label="extra small" initials="xs" />
+      <Avatar data-size="xs" aria-label="extra small" />
+      <Avatar data-size="sm" aria-label="small" initials="sm" />
+      <Avatar data-size="sm" aria-label="small" />
+      <Avatar data-size="md" aria-label="medium" initials="md" />
+      <Avatar data-size="md" aria-label="medium" />
       <Avatar
-        data-size='lg'
-        data-color='brand1'
-        aria-label='large'
-        initials='lg'
+        data-size="lg"
+        data-color="extra1"
+        aria-label="large"
+        initials="lg"
       />
-      <Avatar data-size='lg' variant='square' aria-label='large' />
+      <Avatar data-size="lg" variant="square" aria-label="large" />
     </div>
   );
 };
--- src/pages/components/samples/Tag.tsx
+++ src/pages/components/samples/Tag.tsx
@@ -1,15 +1,15 @@
-import { Tag } from '@digdir/designsystemet-react';
+import { Tag } from "@digdir/designsystemet-react";
 
 function TagSample() {
   return (
-    <div className='row'>
-      <Tag data-color='accent' data-size='sm'>
+    <div className="row">
+      <Tag data-color="accent" data-size="sm">
         sm accent
       </Tag>
-      <Tag data-color='brand1' data-size='md'>
+      <Tag data-color="extra1" data-size="md">
         md brand1
       </Tag>
-      <Tag data-color='warning' data-size='lg'>
+      <Tag data-color="warning" data-size="lg">
         lg warning
       </Tag>
     </div>
